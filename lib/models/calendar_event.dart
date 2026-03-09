a162b71db4b73c0f945fde38d1bd035036992f0a
João Lucas Pinheiro <lucasdallas25@gmail.com>
2026-03-08 21:07:55 -0300

    Inicialização das calsses para o calendario

---
+++ lib/models/calendar_event.dart
@@ -0,0 +1,103 @@
+
+enum CalendarEventType{
+  prova('prova', 'Prova'),
+  trabalho('trabalho','Trabalho'),
+  seminario('seminario','Seminario'),
+  evento('evento','Evento'),
+  academico('academico','academico'),
+  pessoal('pessoal','Pessoal');
+
+  final String value;
+  final String label;
+
+  const CalendarEventType(this.value, this.label);
+  static CalendarEventType fromValue(String value){
+    return CalendarEventType.values.firstWhere((e) => e.value == value,
+    orElse: () => CalendarEventType.pessoal,);
+  }
+}
+
+class CalendarEvent {
+  final String id;
+  final String title;
+  final String? description;
+  final DateTime date;
+  final CalendarEventType type;
+  final String? course;
+  final String authorId;
+  final String authorName;
+  final bool isPersonal;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  CalendarEvent({
+    required this.id,
+    required this.title,
+    this.description,
+    required this.date,
+    required this.type,
+    this.course,
+    required this.authorId,
+    required this.authorName,
+    required this.isPersonal,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  /// Transforma o objeto em um Map (JSON)
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'description': description,
+        'date': date.toIso8601String(),
+        'type': type.value,
+        'course': course,
+        'authorId': authorId,
+        'authorName': authorName,
+        'isPersonal': isPersonal,
+        'createdAt': createdAt.toIso8601String(),
+        'updatedAt': updatedAt.toIso8601String(),
+      };
+
+  /// Cria um objeto a partir de um Map (JSON)
+  factory CalendarEvent.fromJson(Map<String, dynamic> json) => CalendarEvent(
+        id: json['id'] as String,
+        title: json['title'] as String,
+        description: json['description'] as String?,
+        date: DateTime.parse(json['date'] as String),
+        type: CalendarEventType.fromValue(json['type'] as String),
+        course: json['course'] as String?,
+        authorId: json['authorId'] as String,
+        authorName: json['authorName'] as String,
+        isPersonal: json['isPersonal'] as bool,
+        createdAt: DateTime.parse(json['createdAt'] as String),
+        updatedAt: DateTime.parse(json['updatedAt'] as String),
+      );
+/// Quando um dado é atualizado muda o componente para as novas informações sem precisar buscar no banco para a atualização
+  CalendarEvent copyWith({
+    String? id,
+    String? title,
+    String? description,
+    DateTime? date,
+    CalendarEventType? type,
+    String? course,
+    String? authorId,
+    String? authorName,
+    bool? isPersonal,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) =>
+      CalendarEvent(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        date: date ?? this.date,
+        type: type ?? this.type,
+        course: course ?? this.course,
+        authorId: authorId ?? this.authorId,
+        authorName: authorName ?? this.authorName,
+        isPersonal: isPersonal ?? this.isPersonal,
+        createdAt: createdAt ?? this.createdAt,
+        updatedAt: updatedAt ?? this.updatedAt,
+      );
+}
---
+++ lib/models/class_schedule.dart
@@ -0,0 +1,91 @@
+enum AppDay{
+  segunda('segunda', 'Segunda-feira'),
+  terca('terca','Terça-feira'),
+  quarta('quarta', 'Quarta-feira'),
+  quinta('quinta', 'Quinta-feira'),
+  sexta('sexta', 'Sexta-feira'),
+  sabado('sabado', 'Sabado'),
+  domingo('domingo','Domingo');
+
+  final String value;
+  final String label;
+
+  const AppDay(this.value, this.label);
+
+  static AppDay fromValue(String value){
+    return AppDay.values.firstWhere((d) => d.value == value, orElse: () => AppDay.segunda
+    );
+  }
+}
+
+class ClassSchedule {
+  final String id;
+  final AppDay dayOfWeek;
+  final String startTime; 
+  final String endTime;  
+  final String subject;
+  final String professor;
+  final String room;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  ClassSchedule({
+    required this.id,
+    required this.dayOfWeek,
+    required this.startTime,
+    required this.endTime,
+    required this.subject,
+    required this.professor,
+    required this.room,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'dayOfWeek': dayOfWeek.value,
+        'startTime': startTime,
+        'endTime': endTime,
+        'subject': subject,
+        'professor': professor,
+        'room': room,
+        'createdAt': createdAt.toIso8601String(),
+        'updatedAt': updatedAt.toIso8601String(),
+      };
+  factory ClassSchedule.fromJson(Map<String, dynamic> json) => ClassSchedule(
+        id: json['id'] as String,
+        dayOfWeek: AppDay.fromValue(json['type'] as String),
+        startTime: json['startTime'] as String,
+        endTime: json['endTime'] as String,
+        subject: json['subject'] as String,
+        professor: json['professor'] as String,
+        room: json['room'] as String,
+        createdAt: DateTime.parse(json['createdAt'] as String),
+        updatedAt: DateTime.parse(json['updatedAt'] as String),
+      );
+
+
+  ClassSchedule copyWith({
+    String? id,
+    AppDay? dayOfWeek,
+    String? startTime,
+    String? endTime,
+    String? subject,
+    String? professor,
+    String? room,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) =>
+      ClassSchedule(
+        id: id ?? this.id,
+        dayOfWeek: dayOfWeek ?? this.dayOfWeek,
+        startTime: startTime ?? this.startTime,
+        endTime: endTime ?? this.endTime,
+        subject: subject ?? this.subject,
+        professor: professor ?? this.professor,
+        room: room ?? this.room,
+        createdAt: createdAt ?? this.createdAt,
+        updatedAt: updatedAt ?? this.updatedAt,
+      );
+}
